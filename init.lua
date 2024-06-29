0f47f8865f0118dbe6fb66b22303685a29eef92d
Andrew White <andrew.white@devotion.com.au>
2024-06-30 08:44:52 +1000

    Enable typescript language server

--- init.lua
+++ init.lua
@@ -620,6 +620,7 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
+        tsserver = {},
 
         lua_ls = {
           -- cmd = {...},
@@ -650,6 +651,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'typescript-language-server',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
