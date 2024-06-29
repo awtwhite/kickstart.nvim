7dbe3368cb20fd3293118e36c76e961661c4c299
Andrew White <andrew.white@devotion.com.au>
2024-06-30 08:45:59 +1000

    Enable tailwind language server

--- init.lua
+++ init.lua
@@ -636,6 +636,8 @@ require('lazy').setup({
             },
           },
         },
+
+        tailwindcss = {},
       }
 
       -- Ensure the servers and tools above are installed
@@ -652,6 +654,7 @@ require('lazy').setup({
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
         'typescript-language-server',
+        'tailwindcss-language-server',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
